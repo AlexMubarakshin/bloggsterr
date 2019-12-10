8c79e538428ae5470bf5a51c0e618bf8412c194a
Alex Mubarakshin <muban96@mail.ru>
2019-12-10 13:40:04 +0300

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# 👻 Yet another personal site 🤪
+
+  Personal site on Gatsby
+
+## Installation
+
+```shell
+git clone https://github.com/AlexMubarakshin/yet-another-repo.git
+cd ./yet-another-repo
+
+yarn
+
+# Start dev
+yarn start
+
+# Visit localhost:8000
+```
---
+++ src/components/layout.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import Header from './header';
+import './layout.css';
+
+const Layout: React.FC = ({ children }: React.PropsWithChildren<{}>) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {' '}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
